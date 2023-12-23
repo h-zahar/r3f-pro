4bfead33669c3fd0b41414f6d3f1171c584a92a5
h-zahar <hz.rifat@gmail.com>
2023-12-24 04:34:04 +0600

    Added Sizing and Lighting

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import "./App.css";
 function App() {
   return (
     <Canvas>
+      <directionalLight position={[0, 0, 2]} />
       <mesh>
-        <boxGeometry />
-        <meshBasicMaterial />
+        <boxGeometry args={[2, 2, 5]} />
+        <meshStandardMaterial color={"orange"} />
       </mesh>
     </Canvas>
   );
